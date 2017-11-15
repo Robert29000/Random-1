4352b6d9cbd7c52e48dcfde46b77f951f1bfdd04
Robert29000 <mayka29000@mail.ru>
2017-11-15 20:38:09 +0300

    Added "*" and "-" buttons

--- app/src/main/java/study/vv/random/MainActivity.java
+++ app/src/main/java/study/vv/random/MainActivity.java
@@ -16,10 +16,35 @@ public class MainActivity extends AppCompatActivity {
     public void plus(View v){
         EditText num1 = (EditText)findViewById(R.id.first_number);
         EditText num2 = (EditText)findViewById(R.id.second_number);
-        TextView res = (TextView)findViewById(R.id.second_number);
+        TextView res = (TextView)findViewById(R.id.result);
         String snum1 = num1.getText().toString();
         String snum2 = num2.getText().toString();
-        res.setText("" + (Integer.parseInt(snum1) +
-                Integer.parseInt(snum2)));
+        res.setText("" + (Double.parseDouble(snum1) +Double.parseDouble(snum2)));
     }
+
+    public void minus(View view) {
+        EditText num1 = (EditText)findViewById(R.id.first_number);
+        EditText num2 = (EditText)findViewById(R.id.second_number);
+        TextView res = (TextView)findViewById(R.id.result);
+        String snum1 = num1.getText().toString();
+        String snum2 = num2.getText().toString();
+        res.setText("" + (Double.parseDouble(snum1) - Double.parseDouble(snum2)));
+    }
+    public void multiple(View view){
+        EditText num1 = (EditText)findViewById(R.id.first_number);
+        EditText num2 = (EditText)findViewById(R.id.second_number);
+        TextView res = (TextView)findViewById(R.id.result);
+        String snum1 = num1.getText().toString();
+        String snum2 = num2.getText().toString();
+        res.setText("" + (Double.parseDouble(snum1) * Double.parseDouble(snum2)));
+    }
+    public void dis(View view){
+        EditText num1 = (EditText)findViewById(R.id.first_number);
+        EditText num2 = (EditText)findViewById(R.id.second_number);
+        TextView res = (TextView)findViewById(R.id.result);
+        String snum1 = num1.getText().toString();
+        String snum2 = num2.getText().toString();
+        res.setText("" + (Double.parseDouble(snum1) / Double.parseDouble(snum2)));
+    }
+
 }
